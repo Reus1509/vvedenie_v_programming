a6218c48017437c148f3ca5d6a9a76089418badc
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-09-28 20:25:10 +0300

    add project_5_hello_my_lover

---
+++ project_5_Hello_my_lover/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "маша")
+{
+    Console.WriteLine("Привет, любимая!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
