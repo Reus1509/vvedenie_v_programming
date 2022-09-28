2cbfadc9befcd8618a3f207c9dbca2e6d1ee31e4
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-09-28 20:26:57 +0300

    add ToLower() in project_5

--- project_5_Hello_my_lover/Program.cs
+++ project_5_Hello_my_lover/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if (username == "маша")
+if (username.ToLower() == "маша")
 {
     Console.WriteLine("Привет, любимая!!");
 }
