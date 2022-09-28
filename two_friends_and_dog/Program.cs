b3e994f7e2337d3c32ceb63fe14fe5804ed65a7d
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-09-28 22:36:05 +0300

    add two_friend_and_dog

---
+++ two_friends_and_dog/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0,  firstFriendSpeed = 1, secondFriendSpeed = 2, dogSpeed = 5, friend = 2;
+double distance = 10000, time = 0;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.Write("Собака пробежит: ");
+Console.Write(count );
+Console.WriteLine("раз.");
+
