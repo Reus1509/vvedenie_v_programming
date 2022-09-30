4f71c98f5775e5419b9cd9018c3815bb3a242a3c
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-09-30 21:00:34 +0300

    add practise

---
+++ seminar_1/practise/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number2 == number1*number1)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1.");
+}
+else
+{
+    Console.WriteLine("Число 2 не является квадратом числа 1.");
+}
