e0e295fddb582ef6b2dc1187294e057fccfdc807
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-09-30 22:02:45 +0300

    add task_3 and task_4

---
+++ seminar_1/task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int a = number * -1;
+if(number < 0){
+    while(number <= a){
+        Console.Write(number);
+        number++;
+    }
+}
+else{
+    while(a <= number)
+    {
+        Console.Write(a);
+        a++;
+    }
+}
+
