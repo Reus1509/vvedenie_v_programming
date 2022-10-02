cf22551d2acb819d05cc6bbddf5ee899aac4750d
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-02 12:32:52 +0300

    add lection_2 task_1 and task_2

---
+++ lection_2_task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+Console.Write("Максимальное число: ");
+
+int a1 = 2;
+int a2 = 4;
+int a3 = 7;
+int b1 = 45;
+int b2 = 5;
+int b3 = 1762;
+int c1 = 1;
+int c2 = 82;
+int c3 = 13;
+
+// int max1 = Max(a1,a2, a3);
+// int max2 = Max(b1,b2, b3);
+// int max3 = Max(c1,c2, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1,a2, a3), Max(b1,b2, b3), Max(c1,c2, c3));
+
+Console.Write(max);
+
---
+++ lection_2_task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Та же задача, но с использованием массивов
+
+Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+Console.Write("Максимальное число: ");
+
+int[] array = {41, 22, 39, 64, 51, 76, 17, 48, 49};
+
+// int max1 = Max(a1,a2, a3);
+// int max2 = Max(b1,b2, b3);
+// int max3 = Max(c1,c2, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(array[0],array[1], array[2]), Max(array[3],array[4], array[5]), Max(array[6],array[7], array[8]));
+
+Console.Write(max);
+
+
