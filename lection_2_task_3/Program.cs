27fa0172867f7b7f3b9f20fbc8e5281c1cc7fadf
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-02 12:51:29 +0300

    add lection_2 task_3 task_4

---
+++ lection_2_task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//код на константных значениях.
+
+Console.Clear();
+int[] array = {13, 21, 33, 24, 25, 33, 17, 68};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while(index < n){
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
