7174140819e59f2592bd2a6c1c62732ea11398a0
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-02 13:21:48 +0300

    add task_4

--- lection_2_task_4/Program.cs
+++ lection_2_task_4/Program.cs
@@ -1,18 +1,46 @@
 ﻿//код именен с констант
 
 Console.Clear();
-int[] array = {13, 21, 33, 24, 25, 33, 17, 68};
-
-int n = array.Length;
-int find = 33;
-
-int index = 0;
-
-while(index < n){
-    if(array[index] == find)
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
     {
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next(1,10);
+        index++;
     }
-    index++;
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count){
+        if(collection[index] == find){
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
