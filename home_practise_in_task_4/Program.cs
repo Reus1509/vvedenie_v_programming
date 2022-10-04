74030ce4e9083999797328ea1e8bf13dfe73b1e0
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-04 20:16:39 +0300

    add new files home practise

---
+++ home_practise_in_task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+void AddElements(int[] array)
+{
+    int length = array.Length;
+    int count = 0;
+    while(count < length){
+        array[count] = new Random().Next(1,10);
+        count++;
+    }
+}
+void PrintMassive(int [] array){
+    int length = array.Length;
+    int count = 0;
+    while(count < length){
+        Console.Write(array[count] + " ");
+        count++;
+    }
+}
+int IndexOf(int[] array){
+    
+}
+
+int[] array = new int[10];
+
+AddElements(array);
+PrintMassive(array);
