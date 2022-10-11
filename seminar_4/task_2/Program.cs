8ccdf20e9dcae96396cc60e966a8493377ee1a67
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-11 20:55:41 +0300

    add some changes

--- seminar_4/task_2/Program.cs
+++ seminar_4/task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(NumberProizved(number));
+
+int NumberProizved(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
