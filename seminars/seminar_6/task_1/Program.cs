6f79aa54dc737979fd3fdc59e84b34d05086a8d8
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-18 21:40:02 +0300

    add seminar_6

---
+++ seminars/seminar_6/task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 39: Напишите программу, которая перевернёт 
+// одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+Console.Clear();
+int[] array = GetArray(10, 0, 10);
+PrintArray(array);
+RevArray2(array);
+Console.WriteLine();
+PrintArray(array);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void RevArray1(int[] array)
+{
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        int k = array[i];
+        array[i] = array[array.Length-1-i];
+        array[array.Length-1-i] = k;
+    }
+}
+
+int[] RevArray2(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i]=array[array.Length -1 -i];
+    }
+    return result;
+}
---
+++ seminars/seminar_6/task_3/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+void Main()
+{
+    Console.Clear();
+    int number = GetNumber();
+    int[] result = InBinary(number);
+    WriteArray(RevArray(result));
+}
+
+
+int GetNumber()
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] InBinary(int number)
+{
+    int i = 0;
+    int a = 0;
+    int[] array = new int[0];
+    while (number >= 1)
+    {
+        Array.Resize(ref array, array.Length + 1);
+        a = number % 2;
+        array[i] = a;
+        i++;
+
+        number = number / 2;
+    }
+    return array;
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] RevArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int k = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = k;
+    }
+    return array;
+}
+
+Main();
