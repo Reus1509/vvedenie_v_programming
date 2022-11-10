1bdb398f67e13870a1929dde405d49a63fa0fc5e
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-11-10 22:28:55 +0300

    add seminar_9

---
+++ seminars/seminar_9/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    int result = SumElements(number);
+    Console.Write(result);
+}
+
+int SumElements(int number)
+{
+    if(number == 0) return 0;
+    return number % 10 + SumElements(number / 10);
+}
+
+Main();
---
+++ seminars/seminar_9/task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int count = 0;
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int degree = int.Parse(Console.ReadLine());
+int result = DegNumber(number, degree);
+Console.Write(result);
+
+
+int DegNumber(int number, int degree)
+{   
+    if(count == degree) return 1;  
+    count++;
+    return number * DegNumber(number, degree);
+}
+
